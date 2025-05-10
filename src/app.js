9fe6ab70512c17267fa8474d49009736b6c8a9d5
Cristhian Rodríguez <crisrodam1996@gmail.com>
2025-05-10 11:46:07 -0500

    Comunicación de las rutas con el server

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import express, { json } from 'express'
 import cors from 'cors'
 import logger from 'morgan'
-
+import rootRouter from './routes/index.routes.js'
 const app = express()
 
 app.use(cors())
 app.use(logger('dev'))
 app.use(json({ limit: '50mb' }))
+
+// rutas
+app.use('/api/v1', rootRouter)
 
 export default app
